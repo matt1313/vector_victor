fffa71ad5184d3a6b455d50cf9f0b5e4ef3df7f8
Matt Joyce <mattjoyce@Matts-MBP.PK5001Z>
2016-11-03 00:44:16 -0400

    testy functions for vectors

---
+++ vector_victor.py
@@ -0,0 +1,42 @@
+
+vec_a = []
+vec_b = []
+
+def shape(line):
+    #angle based on vert and horz values in list?
+    #No idea here.
+    pass
+
+def vector_add():
+    plus_vec += (vec_a + vec_b)
+    return plus_vec
+
+def vector_sub():
+    minus_vec += (vec_a - vec_b)
+    return minus_vec
+
+def vector_sum():
+    pass
+
+def dot():
+    pass
+
+def vector_multiply():
+    scalar = s
+    vec_a[:], vec_b[:] = [x * s for x in vec_a, vec_b]
+    #multiply values within each vector list/tuple
+    return
+
+def vector_mean():
+    #average the horz values and the vert values in the lists to find a mean horz, vert vector
+    #for num in list, add all list elements and divide them by number of list elements.
+    #^^^Can't use for...damn. Pass for now.
+    pass
+
+def magnitude():
+    pass
+#vectors have both magnitude and direction
+#speed is not a vector quantity, it is only a magnitude value
+#speed is a scaler
+#vector must specify direction
+#2-tuple, or an ordered list of two numbers
